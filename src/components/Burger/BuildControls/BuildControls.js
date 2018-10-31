4ce95303ec45a621eb5d8259410cfc6866e2f10e
Kenneth Mutai <mutaikenn@gmail.com>
2018-10-31 05:25:51 +0200

    Bug Fixing

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -20,7 +20,7 @@ const buildControls = props => (
         key={ctrl.label}
         label={ctrl.label}
         added={() => props.ingredientAdded(ctrl.type)}
-        remove={() => props.ingredientRemoved(ctrl.type)}
+        removed={() => props.ingredientRemoved(ctrl.type)}
         disabled={props.disabled[ctrl.type]}
       />
     ))}
